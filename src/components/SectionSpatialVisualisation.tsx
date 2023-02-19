815bec38f06bea31adfaa87cf73018db490279f4
Fabian <fabian@aimermedia.com>
2023-02-19 19:22:58 +0000

    added letters for spatial visualisation section

--- src/components/SectionSpatialVisualisation.tsx
+++ src/components/SectionSpatialVisualisation.tsx
@@ -23,6 +23,31 @@ function SectionSpatialVisualisation({
 
   return (
     <div className="bg-primary h-screen flex flex-col justify-center align-middle">
+      <div className="flex justify-center mb-6 gap-5 text-center">
+        {currentQuestion.letters?.map(letter => {
+          const firstLetter = letter[0];
+          const secondLetter = letter[1];
+          //   TODO using in-line styling here instead of tailwind :(
+          return (
+            <div className="text-7xl text-black bg-cream p-7 rounded font-semibold">
+              <div
+                style={{
+                  transform: `rotate(${firstLetter.rot}deg) ${firstLetter.flip ? "scale(-1, 1)" : "scale(1, 1)"}`,
+                }}
+              >
+                {firstLetter.char}
+              </div>
+              <div
+                style={{
+                  transform: `rotate(${secondLetter.rot}deg) ${secondLetter.flip ? "scale(-1, 1)" : "scale(1, 1)"}`,
+                }}
+              >
+                {secondLetter.char}
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <Choices choices={currentQuestion.choices} handleAnswerClick={handleAnswerClick} />
     </div>
   );
