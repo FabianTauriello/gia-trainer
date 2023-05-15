0c5f2e140146f7829b68926045bcb8bd204c41d5
Fabian <fabian@aimermedia.com>
2023-05-15 22:59:17 +0100

    make link customizable

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
 
-export function CustomLink({ text, iconClasses, customCss = "" }: { text: string; iconClasses?: string; customCss?: string }) {
+export function CustomLink({
+  text,
+  iconClasses,
+  location,
+  customCss = "",
+}: {
+  text: string;
+  iconClasses?: string;
+  location: string;
+  customCss?: string;
+}) {
   return (
     <Link
-      to={"/quiz/visitor"}
+      to={location}
       className={`cursor-pointer select-none rounded-lg p-4 font-bold transition-transform hover:scale-105 ${customCss}`}
     >
       <span>{text}</span>
