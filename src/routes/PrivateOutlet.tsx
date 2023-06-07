0fad7557f0d1e20aca927423a19e5459df03bd27
Fabian <fabian@aimermedia.com>
2023-06-07 10:41:45 +0100

    reworked private outlet

--- src/routes/PrivateOutlet.tsx
+++ src/routes/PrivateOutlet.tsx
@@ -1,8 +1,8 @@
-import { useAuth } from "hooks/useAuth";
+import { useAppSelector } from "hooks/useAppSelector";
 import { useLocation, Outlet, Navigate } from "react-router-dom";
 
 export function PrivateOutlet() {
-  const auth = useAuth();
+  const auth = useAppSelector((state) => state.auth);
   const location = useLocation();
 
   return auth.user ? <Outlet /> : <Navigate to="/sign-in" state={{ from: location }} />;
