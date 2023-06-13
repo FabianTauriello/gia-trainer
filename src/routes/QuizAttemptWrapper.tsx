5ca506f0fcd05e246af1bc1e692150203957f97a
Fabian <fabian@aimermedia.com>
2023-06-13 20:54:11 +0100

    improved loading and error states for quiz question fetching

--- src/routes/QuizAttemptWrapper.tsx
+++ src/routes/QuizAttemptWrapper.tsx
@@ -3,16 +3,48 @@ import { useAppDispatch } from "hooks/useAppSelector";
 import { useGetQuizQuestionsQuery } from "domain/slices/apislice";
 import { QuestionController } from "components/QuestionController";
 import QuizContextProvider from "domain/QuizContextProvider";
+import { CategoryIntro } from "components/CategoryIntro";
+import { Banner } from "components/Banner";
+import { CustomButton } from "components/CustomButton";
 
 export function QuizAttemptWrapper() {
   const dispatch = useAppDispatch();
 
-  // TODO handle error here and maybe use lazy hook version
-  const { data: questions, isError, error, isLoading, isFetching } = useGetQuizQuestionsQuery();
+  const { data: questions, isError, error, isLoading, isFetching, refetch } = useGetQuizQuestionsQuery();
 
-  if (isLoading) return <div>loading...</div>;
+  if (isLoading)
+    return (
+      <div className="h-screen bg-slate-200 dark:bg-slate-900 dark:text-white">
+        <Banner title="...loading your quiz questions" />
+        <div className="animate-pulse px-4 py-8 md:px-0 lg:mx-28 grid grid-cols-4 gap-4">
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-3" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-1" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-2" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-2" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-1" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-3" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-4" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-1" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-2" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-1" />
+          <div className="h-5 bg-slate-400 dark:bg-slate-700 rounded col-span-2" />
+          <div className="h-8 mt-8 bg-slate-400 dark:bg-slate-700 rounded col-span-4" />
+        </div>
+      </div>
+    );
 
-  if (isError) return <div>error retrieving latest quiz questions</div>;
+  if (isError)
+    return (
+      <div className="h-screen bg-slate-200 dark:bg-slate-900 dark:text-white">
+        <Banner title="Error" />
+        <div className="px-4 md:px-0 lg:mx-28">
+          <div className="py-8">
+            <p>There was an error retrieving the latest quiz questions. Please Try again by clicking the button below.</p>
+          </div>
+          <CustomButton onClick={refetch}>Load Questions</CustomButton>
+        </div>
+      </div>
+    );
 
   return (
     <QuizContextProvider allQuestions={questions!}>
