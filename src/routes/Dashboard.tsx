47e80cce3cfd7b2b8f5a7ee2f22e6d11018b71d2
Fabian T <ftauriello@hotmail.com>
2023-06-26 21:02:33 +0100

    added label to notify users dashboard is under construction

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -7,11 +7,11 @@ function Dashboard() {
   const auth = useAppSelector((state) => state.auth);
 
   return (
-    <div>
-      <h1>dash</h1>
-      <h2>
+    <div className="flex items-center justify-center border border-black">
+      <h1>Dashboard is under construction</h1>
+      {/* <h2>
         <pre>{JSON.stringify(auth, null, 4)}</pre>
-      </h2>
+      </h2> */}
     </div>
   );
 }
