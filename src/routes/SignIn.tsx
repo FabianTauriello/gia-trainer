90bb82ec5396bb1581006ac4daf92d0af88ba298
Fabian T <ftauriello@hotmail.com>
2023-07-27 18:43:59 +0100

    changed eye icon again

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -4,7 +4,7 @@ import { useAppDispatch, useAppSelector } from "hooks/useAppSelector";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CustomButton } from "components/CustomButton";
-import { HiEye, HiEyeSlash } from "react-icons/hi2";
+import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi";
 import { Utils } from "utils/Utils";
 import logo from "../assets/svgs/logo.svg";
 
@@ -184,7 +184,7 @@ export function SignIn() {
                   onClick={() => setShowPassword(!showPassword)}
                   className="tooltip absolute inset-y-0 right-4 flex items-center pl-3"
                 >
-                  {showPassword ? <HiEyeSlash /> : <HiEye />}
+                  {showPassword ? <HiOutlineEyeOff /> : <HiOutlineEye />}
                 </button>
               </div>
             </div>
