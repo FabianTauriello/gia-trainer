830dd05bfed0c99e7f86e6793b5d3f3869bc0330
Fabian T <ftauriello@hotmail.com>
2023-07-30 23:30:20 +0100

    removed import

--- src/routes/PrivateOutlet.tsx
+++ src/routes/PrivateOutlet.tsx
@@ -1,11 +1,9 @@
 import { useAppSelector } from "hooks/useAppSelector";
 import { useLocation, Outlet, Navigate } from "react-router-dom";
-import Dashboard from "./Dashboard";
 
 export function PrivateOutlet() {
   const auth = useAppSelector((state) => state.auth);
   const location = useLocation();
 
   return auth.user ? <Outlet /> : <Navigate to="/sign-in" state={{ from: location }} />;
-  // return auth.user ? <Dashboard /> : <Navigate to="/sign-in" state={{ from: location }} />;
 }
