529c9f500d5a22cef6a2f4affca48904dc00bf6d
Fabian T <ftauriello@hotmail.com>
2023-08-30 23:44:01 +0100

    added react-table dep and began work on attempts table for dash

--- src/components/DashboardAttempts.tsx
+++ src/components/DashboardAttempts.tsx
@@ -1,12 +1,53 @@
 import { useGetQuizAttemptsQuery } from "domain/slices/apislice";
 import { useAppSelector } from "hooks/useAppSelector";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { DataTable } from "./DataTable";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const columns: ColumnDef<{
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "completed",
+    header: "Completed",
+  },
+];
 
 export function DashboardAttempts() {
   const { quiz, auth } = useAppSelector((state) => state);
 
   const { data, isError, isLoading, isFetching, refetch, isSuccess } = useGetQuizAttemptsQuery(auth.user!.id);
+
+  const [postData, setPostData] = useState<Post[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      console.log("res: ", res);
+      if (res.ok) {
+        const json = await res.json();
+        setPostData(json);
+      }
+    } catch (error) {
+      console.log("Failed to get post data");
+    }
+  }
 
   function getTime(time: string) {
     return new Date(time).toLocaleString();
@@ -15,13 +56,7 @@ export function DashboardAttempts() {
   return (
     <div className="dashContentContainer">
       <h1>Attempts list</h1>
-      <pre>
-        {data?.data
-          ? data?.data.map((i, index) => {
-              return <div key={index}>{getTime(i.timestamp)}</div>;
-            })
-          : null}
-      </pre>
+      {postData ? <DataTable columns={columns} data={postData} /> : null}
     </div>
   );
 }
