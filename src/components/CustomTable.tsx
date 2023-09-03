f55844a0f930f7bdfb1fec18acbcf9c3b5de890c
Fabian T <ftauriello@hotmail.com>
2023-09-03 01:52:18 +0100

    improved attempts table styling

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -1,5 +1,6 @@
 import { ColumnDef, flexRender, getCoreRowModel, useReactTable, SortingState, getSortedRowModel } from "@tanstack/react-table";
 import { useState } from "react";
+import { BiSolidCaretUpSquare, BiSolidCaretDownSquare } from "react-icons/bi";
 
 type CustomTableProps<TData, TValue> = {
   columns: ColumnDef<TData, TValue>[];
@@ -21,24 +22,26 @@ export function CustomTable<TData, TValue>({ columns, data, handleRowClick }: Cu
   });
 
   return (
-    <table className="w-full border dark:border-slate-700 border-slate-300">
+    <table className="w-full border-2 border-slate-200 dark:border-slate-700">
       <thead className="bg-white dark:bg-darkSlate">
         {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
+          <tr key={headerGroup.id} className="border-2 border-b-slate-200 dark:border-slate-700 border-t-0 border-x-0">
             {headerGroup.headers.map((header) => (
-              <th key={header.id} className="text-left p-3">
+              <th key={header.id} className="text-left p-3 border border-l-slate-200 dark:border-slate-700 border-r-0 border-y-0">
                 {header.isPlaceholder ? null : (
                   <div
                     {...{
-                      className: header.column.getCanSort() ? "cursor-pointer select-none" : "",
+                      className: header.column.getCanSort() ? "cursor-pointer select-none flex justify-between" : "",
                       onClick: header.column.getToggleSortingHandler(),
                     }}
                   >
                     {flexRender(header.column.columnDef.header, header.getContext())}
-                    {{
-                      asc: " 🔼",
-                      desc: " 🔽",
-                    }[header.column.getIsSorted() as string] ?? null}
+                    <span className="flex justify-center items-center" style={{ width: "24px", textAlign: "center" }}>
+                      {{
+                        asc: <BiSolidCaretUpSquare size={20} />,
+                        desc: <BiSolidCaretDownSquare size={20} />,
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </span>
                   </div>
                 )}
               </th>
@@ -47,21 +50,31 @@ export function CustomTable<TData, TValue>({ columns, data, handleRowClick }: Cu
         ))}
       </thead>
       <tbody>
-        {table.getRowModel().rows.map((row, i) => (
-          <tr
-            key={row.id}
-            className="dark:even:bg-slate-900 even:bg-white hover:dark:bg-slate-700 hover:bg-slate-200 cursor-pointer"
-            onClick={handleRowClick}
-          >
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id} className="p-3">
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
+        {data.length ? (
+          <>
+            {table.getRowModel().rows.map((row, i) => (
+              <tr
+                key={row.id}
+                className="dark:even:bg-slate-900 even:bg-white hover:dark:bg-slate-700 hover:bg-slate-200 cursor-pointer"
+                onClick={handleRowClick}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="p-3 border border-l-slate-200 dark:border-l-slate-700 border-r-0 border-y-0">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
             ))}
+          </>
+        ) : (
+          <tr className="">
+            <td colSpan={3} className="p-4 text-center">
+              No results
+            </td>
           </tr>
-        ))}
+        )}
       </tbody>
-      <pre>{JSON.stringify(sorting, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(sorting, null, 2)}</pre> */}
     </table>
   );
 }
--- src/components/DashboardAttempts.tsx
+++ src/components/DashboardAttempts.tsx
@@ -1,12 +1,13 @@
 import { useGetQuizAttemptsQuery } from "domain/slices/apislice";
 import { useAppSelector } from "hooks/useAppSelector";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { ColumnDef } from "@tanstack/react-table";
 import { QuizAttempt } from "domain/Types";
 import { CustomTable } from "./CustomTable";
 import { PiCaretRightBold, PiCaretLeftBold } from "react-icons/pi";
 import { Utils } from "utils/Utils";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export const columns: ColumnDef<QuizAttempt>[] = [
   {
@@ -14,7 +15,6 @@ export const columns: ColumnDef<QuizAttempt>[] = [
     header: "Id",
   },
   {
-    // accessorFn: row => row.timestamp,
     accessorKey: "timestamp",
     header: "Timestamp",
     cell: (props) => <span>{Utils.formatTimestamp(props.getValue<string>())}</span>,
@@ -25,6 +25,7 @@ export const columns: ColumnDef<QuizAttempt>[] = [
   },
 ];
 
+// TODO add loading indicator for pagination
 export function DashboardAttempts() {
   const { quiz, auth } = useAppSelector((state) => state);
 
@@ -39,29 +40,49 @@ export function DashboardAttempts() {
     isSuccess,
   } = useGetQuizAttemptsQuery({ userId: auth.user!.id, pageInfo: { page: page, limit: 20 } });
 
+  const dataFound = attemptsResponse?.data?.attempts.length;
+
   return (
     <div className="dashContentContainer">
       <div className="card p-4">
-        <div className="flex justify-between items-center">
-          <h1 className="">Attempts</h1>
-          <div>
-            <button
-              className="bg-slate-300 dark:bg-slate-900 rounded-md p-1 disabled:opacity-50 border border-slate-200 dark:border-slate-700"
-              disabled={page === 1}
-              onClick={() => setPage(page - 1)}
-            >
-              <PiCaretLeftBold size={18} />
-            </button>
-            <button
-              className="bg-slate-300 dark:bg-slate-900 rounded-md p-1 disabled:opacity-50 border border-slate-200 dark:border-slate-700"
-              disabled={page === attemptsResponse?.data?.totalPages}
-              onClick={() => setPage(page + 1)}
-            >
-              <PiCaretRightBold size={18} />
-            </button>
+        <div className="flex justify-between flex-wrap">
+          <Select defaultValue="all attempts">
+            <SelectTrigger className="w-[180px] dark:bg-slate-700">
+              <SelectValue className="" />
+            </SelectTrigger>
+            <SelectContent className="dark:bg-slate-700">
+              <SelectItem value="all attempts">All Attempts</SelectItem>
+              {/* TODO have other select items for when a user can complete an individual category */}
+              {/* <SelectItem value="reasoning">Reasoning</SelectItem> */}
+              {/* <SelectItem value="perceptual speed">Perceptual Speed</SelectItem> */}
+              {/* <SelectItem value="number speed and accuracy">Number Speed and Accuracy</SelectItem> */}
+              {/* <SelectItem value="word meaning">Word Meaning</SelectItem> */}
+              {/* <SelectItem value="spatial visualisation">Spatial Visualisation</SelectItem> */}
+            </SelectContent>
+          </Select>
+          <div className="flex flex-col items-end">
+            <div className="flex items-center">
+              {dataFound && <span className="text-sm mr-2">{`Page ${page} of ${attemptsResponse?.data?.totalPages}`}</span>}
+              <button
+                className="bg-slate-300 dark:bg-slate-900 rounded-md p-1 disabled:opacity-50 border border-slate-200 dark:border-slate-700"
+                disabled={page === 1}
+                onClick={() => setPage(page - 1)}
+              >
+                <PiCaretLeftBold size={18} />
+              </button>
+              <button
+                className="bg-slate-300 dark:bg-slate-900 rounded-md p-1 disabled:opacity-50 border border-slate-200 dark:border-slate-700"
+                disabled={page === attemptsResponse?.data?.totalPages}
+                onClick={() => setPage(page + 1)}
+              >
+                <PiCaretRightBold size={18} />
+              </button>
+            </div>
+            <span className="text-xs my-1 text-slate-500 dark:text-slate-400">
+              {attemptsResponse?.data?.totalAttemptsCount ?? 0} results
+            </span>
           </div>
         </div>
-        <p>{attemptsResponse?.data?.totalAttemptsCount ?? 0} results</p>
         <CustomTable columns={columns} data={attemptsResponse?.data?.attempts ?? []} handleRowClick={() => {}} />
       </div>
     </div>
