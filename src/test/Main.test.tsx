e6901e93447c3872b39987df30b1bf38376033df
Fabian T <ftauriello@hotmail.com>
2023-09-14 11:19:53 +0100

    updated vite

--- src/test/Main.test.tsx
+++ src/test/Main.test.tsx
@@ -1,28 +1,25 @@
 import { render, screen } from "@testing-library/react";
-// import { Landing } from "routes/Landing";
-// import { ThemeContainer } from "components/ThemeContainer";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistor, store } from "domain/store";
-// import { Provider } from "react-redux";
-// import { AppRouter } from "AppRouter";
-// import ReactDOM from "react-dom/client";
-import { App } from "./app";
-// import "./index.css";
-
-// test("sign-in", () => {
-//   render(
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <ThemeContainer>
-//           <AppRouter />
-//         </ThemeContainer>
-//       </PersistGate>
-//     </Provider>
-//   );
-//   expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("");
-// });
+import { ThemeContainer } from "components/ThemeContainer";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "domain/store";
+import { Provider } from "react-redux";
+import { AppRouter } from "AppRouter";
+import { TestComponent } from "./TestComponent";
 
 test("sign-in", () => {
-  render(<App />);
-  expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("hello world");
+  render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeContainer>
+          <AppRouter />
+        </ThemeContainer>
+      </PersistGate>
+    </Provider>
+  );
+  expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("");
+});
+
+test("TestComponent", () => {
+  render(<TestComponent />);
+  expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("Hello World");
 });
