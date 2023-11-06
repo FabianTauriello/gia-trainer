d41c6ea9823a2b65ebffc6ec75a5d8c2a0c6516f
Fabian T <ftauriello@hotmail.com>
2023-11-06 23:42:46 +0000

    added more tests for sign-in

--- src/tests/SignIn.test.tsx
+++ src/tests/SignIn.test.tsx
@@ -7,7 +7,7 @@ import { describe } from "vitest";
 describe("SignIn", () => {
   test("Email input receives focus", async () => {
     user.setup();
-    customRender(<SignIn />, {});
+    customRender(<SignIn />);
     const emailLabel = screen.getByText(/email/i);
     const emailInput = screen.getByRole("textbox", {
       name: /email/i,
@@ -19,7 +19,7 @@ describe("SignIn", () => {
 
   test("Password input receives focus", async () => {
     user.setup();
-    customRender(<SignIn />, {});
+    customRender(<SignIn />);
     const passwordLabel = screen.getByText(/password/i);
     const passwordInput = screen.getByLabelText(/Password/); // Inputs with a 'password' type don't have a role so use getByLabelText() instead
     await user.click(passwordLabel);
@@ -28,8 +28,7 @@ describe("SignIn", () => {
   });
 
   test("Password input type default is password", async () => {
-    user.setup();
-    customRender(<SignIn />, {});
+    customRender(<SignIn />);
     const passwordInput = screen.getByLabelText(/Password/);
 
     expect(passwordInput).toHaveAttribute("type", "password");
@@ -37,12 +36,39 @@ describe("SignIn", () => {
 
   test("Password input type changes to text", async () => {
     user.setup();
-    customRender(<SignIn />, {});
+    customRender(<SignIn />);
     const togglePasswordVisibilityButton = screen.getByRole("button", { name: /toggle password type/i });
     const passwordInput = screen.getByLabelText(/Password/);
 
     expect(passwordInput).toHaveAttribute("type", "password");
     await user.click(togglePasswordVisibilityButton);
     expect(passwordInput).toHaveAttribute("type", "text");
+  });
+
+  test("Sign-in is default view", () => {
+    customRender(<SignIn />);
+
+    const signInLabel = screen.getByRole("heading", {
+      name: /sign in to your account/i,
+    });
+
+    expect(signInLabel).toBeInTheDocument();
+  });
+
+  test("Sign-in view changes to sign up view", async () => {
+    user.setup();
+    customRender(<SignIn />);
+    const signUpButton = screen.getByText(/sign up/i);
+    const signUpLabel = screen.queryByRole("heading", {
+      name: /create an account/i,
+    });
+
+    expect(signUpLabel).not.toBeInTheDocument();
+
+    await user.click(signUpButton);
+    const signUpLabelAfterClick = screen.getByRole("heading", {
+      name: /create an account/i,
+    });
+    expect(signUpLabelAfterClick).toBeInTheDocument();
   });
 });
