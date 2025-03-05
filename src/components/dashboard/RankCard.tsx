8e555b80afce47a071d6d94f1e61472c9417fef8
Fabian T <ftauriello@hotmail.com>
2025-03-05 22:20:08 +0000

    updated rank card when user doesn't have any attempts recorded

--- src/components/dashboard/RankCard.tsx
+++ src/components/dashboard/RankCard.tsx
@@ -10,35 +10,21 @@ function RankCard() {
     <div className="p-6 flex flex-col rounded-lg card">
       <h1 className="text-lg p-3 font-bold">Rank by Category</h1>
       <ul>
-        {categoryRankings?.data?.map((item, i) => {
-          const even = i % 2 === 0;
-          return (
-            <li className={`p-3 flex justify-between ${even && "bg-slate-200 dark:bg-slate-700"}`}>
-              <div>{item.category}</div>
-              <div className="ml-10">{item.ranking}</div>
-            </li>
-          );
-        })}
-        {/* <li className="p-3 flex justify-between bg-slate-200 dark:bg-slate-700">
-          <div>Reasoning</div>
-          <div className="ml-10">1</div>
-        </li>
-        <li className="p-3 flex justify-between">
-          <div>Perceptual Speed</div>
-          <div className="ml-10">1</div>
-        </li>
-        <li className="p-3 flex justify-between bg-slate-200 dark:bg-slate-700">
-          <div>Number Speed and Accuracy</div>
-          <div className="ml-10">1</div>
-        </li>
-        <li className="p-3 flex justify-between">
-          <div>Word Meaning</div>
-          <div className="ml-10">1</div>
-        </li>
-        <li className="p-3 flex justify-between bg-slate-200 dark:bg-slate-700">
-          <div>Spatial Visualisation</div>
-          <div className="ml-10">1</div>
-        </li> */}
+        {categoryRankings?.data?.length ? (
+          <>
+            {categoryRankings?.data?.map((item, i) => {
+              const even = i % 2 === 0;
+              return (
+                <li className={`p-3 flex justify-between ${even && "bg-slate-200 dark:bg-slate-700"}`}>
+                  <div>{item.category}</div>
+                  <div className="ml-10">{item.ranking}</div>
+                </li>
+              );
+            })}
+          </>
+        ) : (
+          <div className="p-3">No rankings available</div>
+        )}
       </ul>
     </div>
   );
