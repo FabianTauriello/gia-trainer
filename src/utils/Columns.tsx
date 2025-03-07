b59ef034342edb2cd9b56da08d6382790fe22664
Fabian T <ftauriello@hotmail.com>
2025-03-07 18:45:42 +0000

    added columns for leaderboard

--- src/utils/Columns.tsx
+++ src/utils/Columns.tsx
@@ -1,5 +1,5 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { QuizAttempt } from "domain/types";
+import { OverallLeaderboardRecord, QuizAttempt } from "domain/types";
 import { Utils } from "./Utils";
 
 export const attemptsColumns: ColumnDef<QuizAttempt>[] = [
@@ -15,5 +15,47 @@ export const attemptsColumns: ColumnDef<QuizAttempt>[] = [
   {
     accessorKey: "totalScore",
     header: "Total Score",
+  },
+];
+
+export const overallLeaderboardColumns: ColumnDef<OverallLeaderboardRecord>[] = [
+  {
+    accessorKey: "position",
+    header: "Position",
+  },
+  {
+    id: "name",
+    header: "Name",
+    cell: (props) => {
+      const { firstName, lastName, exposeName } = props.row.original;
+      return exposeName ? `${firstName} ${lastName}` : "********";
+    },
+  },
+  {
+    accessorKey: "averageScore",
+    header: "Average Score",
+  },
+  {
+    accessorKey: "averageAccuracy",
+    header: "Average Accuracy",
+  },
+];
+
+export const categoryLeaderboardColumns: ColumnDef<OverallLeaderboardRecord>[] = [
+  {
+    accessorKey: "position",
+    header: "Position",
+  },
+  {
+    id: "name",
+    header: "Name",
+    cell: (props) => {
+      const { firstName, lastName, exposeName } = props.row.original;
+      return exposeName ? `${firstName} ${lastName}` : "********";
+    },
+  },
+  {
+    accessorKey: "averageAccuracy",
+    header: "Average Accuracy",
   },
 ];
